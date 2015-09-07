e52a68536324fc920ca9add31973bac6229f86b5
Francesco Pira <dev@fpira.com>
2015-09-07 09:11:44 +0200

    improved build script to build different doc versions

--- build.py
+++ build.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 
+import sys
 import os
 import re
 import sqlite3
@@ -15,8 +16,9 @@ cur.execute('CREATE TABLE searchIndex(id INTEGER PRIMARY KEY, name TEXT, type TE
 cur.execute('CREATE UNIQUE INDEX anchor ON searchIndex (name, type, path);')
 
 docpath = 'electron.docset/Contents/Resources/Documents/docs/v0.31.0'
-# subpath = 'docs'
-# version = 'v0.31.0'
+subpath = 'docs/'
+version = sys.argv[1]
+docpath = 'electron.docset/Contents/Resources/Documents/'+subpath+version
 
 page = open(os.path.join(docpath, 'index.html')).read()
 soup = BeautifulSoup(page, "html.parser")
@@ -26,6 +28,7 @@ for tag in soup.find_all('a', {'href': any}):
     name = tag.text.strip()
     if len(name) > 0:
         path = tag.attrs['href'].strip()
+        path = subpath+version+'/'+path
         if path.split('#')[0] not in ('index.html'):
             cur.execute('INSERT OR IGNORE INTO searchIndex(name, type, path) VALUES (?,?,?)', (name, 'Guide', path))
             print 'name: %s, path: %s' % (name, path)
@@ -35,6 +38,7 @@ for tag in soup.find_all('a', {'href': any}):
     name = tag.text.strip()
     if len(name) > 0:
         path = tag.attrs['href'].strip()
+        path = subpath+version+'/'+path
         if path.split('#')[0] not in ('index.html'):
             cur.execute('INSERT OR IGNORE INTO searchIndex(name, type, path) VALUES (?,?,?)', (name, 'Guide', path))
             print 'name: %s, path: %s' % (name, path)
@@ -44,6 +48,7 @@ for tag in soup.find_all('a', {'href': any}):
     name = tag.text.strip()
     if len(name) > 0:
         path = tag.attrs['href'].strip()
+        path = subpath+version+'/'+path
         if path.split('#')[0] not in ('index.html'):
             cur.execute('INSERT OR IGNORE INTO searchIndex(name, type, path) VALUES (?,?,?)', (name, 'Module', path))
             print 'name: %s, path: %s' % (name, path)
