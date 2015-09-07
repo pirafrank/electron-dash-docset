dec80870b1b8f82c6f4bb1cd2a07d9d1fcdca98d
Francesco Pira <dev@fpira.com>
2015-09-07 15:34:28 +0200

    pack script now copies also info.plist

--- pack.sh
+++ pack.sh
@@ -2,6 +2,9 @@
 
 echo "copying resources into electron.docset ..."
 cp res/icon* electron.docset/
+cp res/Info.plist electron.docset/Contents/
+
+sleep 1
 
 echo "Start packing..."
 tar --exclude='.DS_Store' -cvzf electron.tgz electron.docset
