8bb1a43ae5f3af94d2279473f53e6cfdcff2033d
Francesco Pira <dev@fpira.com>
2015-09-07 15:34:55 +0200

    added prepare script and license

--- prepare.sh
+++ prepare.sh
@@ -24,11 +24,33 @@
 #
 ########################################################################
 
-echo "copying resources into electron.docset ..."
-cp res/icon* electron.docset/
-cp res/Info.plist electron.docset/Contents/
+# variables
+VERSION="$1"
+DOMAIN="electron.atom.io"
+URL="http://electron.atom.io/docs/""$VERSION""/" # The trailing slash is extremely important!
+
+# checking passed arguments
+if [[ $# != 1 ]]; then
+    echo "Error: wrong number of arguments"
+    echo "Usage: ./prepare.sh <doc version>"
+    echo "Example: ./prepare.sh v0.31.0"
+    echo ""
+    echo "Check the version syntax matches the version in the URL the"
+    echo "For example: If doc URL is http://electron.atom.io/docs/v0.31.0, then doc version is v0.31.0"
+    exit -1
+fi
+
+echo "Creating docset dir..."
+mkdir -p electron.docset/Contents/Resources/Documents
+
+echo "Downloading documentation from electron.atom.io..."
+wget --recursive --no-clobber --page-requisites --html-extension --convert-links --restrict-file-names=windows --domain "$DOMAIN" --no-parent "$URL"
 
 sleep 1
 
-echo "Start packing..."
-tar --exclude='.DS_Store' -cvzf electron.tgz electron.docset
+mv "electron.atom.io/docs/""$VERSION" electron.atom.io/docs/version
+
+sleep 1
+
+echo "copying downloaded files to docset..."
+cp -rf electron.atom.io/ electron.docset/Contents/Resources/Documents/
