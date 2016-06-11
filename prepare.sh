286d8e3a1510266a7f219f95d0088c0d4d73382c
Francesco Pira <dev@fpira.com>
2016-06-11 19:38:49 +0200

    contributor preable moved to separate file

--- prepare.sh
+++ prepare.sh
@@ -1,29 +1,5 @@
 #!/bin/bash
 
-########################################################################
-# electron-dash-docset <https://github.com/pirafrank/battery_life_extender>
-# Notifies the user when plug or unplug the power cord to extend
-# the overall battery life
-#
-# Copyright (C) 2015 Francesco Pira <dev@fpira.com>
-#
-# This file is part of electron-dash-docset.
-#
-# battery_life_extender is free software: you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation, either version 3 of the License, or
-# (at your option) any later version.
-#
-# battery_life_extender is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
-#
-# You should have received a copy of the GNU General Public License
-# along with battery_life_extender. If not, see <http://www.gnu.org/licenses/>.
-#
-########################################################################
-
 # dependencies
 if [[ ! $(which "wget") ]]; then
     echo -e >&2 "\n$(tput setaf 1)This script requires wget. Please install it first.$(tput sgr0)\n"
